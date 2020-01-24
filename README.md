365d8bdebbf88326d7efeabe5066efe63592b7ea
Gabriel Gregório da Silva <48621655+gabrielogregorio@users.noreply.github.com>
2020-01-24 07:03:04 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,88 @@
+# Aviso
+Esse é um projeto experimental apenas para estudos. 
+
+# Como Treinar a rede
+Para fazer o treinamento dessa rede, é necessário ter um conjunto de dados de entradas, relacionando com os dados de saidas.
+
+Também é necessário informar a rede. Vamos direto ao ponto.
+
+´´´python
+
+# Importe a biblioteca de treino
+from neural import Treino
+
+# Defina um intervalo de randomização. Neste caso, todos os pesos da rede terão pesos entre -5 e 5
+intervalorMenor = -5
+intervaloMaior = 5
+
+# Defina a rede, nesse caso, ela tem 2 entradas, 2 camadas no meio e 1 saida
+rede = [2, 2, 1]
+
+# Vai entrar 2 e 4 na rede
+entradas = [2, 4]
+
+# O Resultado esperado é 8
+esperado = [8]
+
+# Serão executadas 1000 redes com pesos aleatórios
+geracoes = 1000
+
+# Instância do treino
+script = Treino(rede, intervalorMenor, intervaloMaior, esperado, entradas, geracoes)
+
+# Retorna a melhor rede dentre as 1000. Execute até encontrar o resultado mais próximo do esperado. Depois teste a rede com outros valores, para garantir que a rede está funcionando
+print(script.treinarRede())
+´´´
+# Como testar uma rede
+´´´python
+
+# Importe a biblioteca
+from neural import RedeNeural
+
+# Informe os pesos, de acordo com os pesos e a rede usada no treinamento
+pesos = [[-6.918, 3.564], [2.09, 3.002], [-0.025]]
+
+# Informe as entradas
+entradas = [16, 32]
+
+# Informe a rede, de acordo com os pesos e a rede usada no treinamento
+rede = [2, 2, 1]
+
+# Crie uma instância passando a rede, a entrada e os pesos
+instancia = RedeNeural(rede, entradas, pesos)
+
+# Exiba a saida associada aos pesos, a rede e as entradas
+print(instancia.iniciar())
+´´´
+
+# Como otimizar a rede
+Uma vez que a rede está se aproximando dos valores esperados, mesmo com multiplos testes, nós precisamos otimizar a rede, para deixa-la mais exata. Como fazer isso?
+
+´´´Python
+# Importe a classe otimizar
+from neural import Otimizar
+
+# Defina uma rede, use a mesma do treino
+rede = [2, 2, 1]
+
+# Defina um resultado esperado, use a mesma do treino
+esperado = [8]
+
+# Defina uma entradas, use a mesma do treino
+entradas = [2, 4]
+
+# Defina o melhor peso, após alguns testes no treino, você deve ter obtido algum peso que funcionou muito bem, use ele.
+melhorPeso = [[-6.918, 3.564], [2.09, 3.002], [-0.025]]
+
+# Define quantas redes serão testadas de forma aleatória
+geracoesOtimizacao = 10000
+
+# Define a regoão da otimização, basicamente, se o seu peso do neurônio vale 10, e a sua otimização por esteja em 100, o peso deste neurônio será escolhido de forma aleatória entre "pesoNeuronio - pesoNeuronio / 100" e "pesoNeuronio + pesoNeuronio / 100" ou vice versa no caso de número negativo. Neste caso, o peso aleatório escolhido seria entre 9,9 e 10,1. Se o seu resultado já está muito bom, mas você pretende otimizar ainda mais, coloque um número bem mais alto para aumentar a precisão
+otimizacaoPor = 1000
+
+# Crie uma instância da otimização
+instancia = Otimizar(rede, esperado, entradas, geracoesOtimizacao, melhorPeso, otimizacaoPor)
+
+# Chame o método e ele retornará o melhor peso otimizado desta execução.
+print(instancia.otimizarRede())
+´´´
--- neural.py
+++ neural.py
@@ -1,36 +1,182 @@
 from math import sqrt
+from random import randint
 
 
 class RedeNeural():
     def __init__(self, rede, entradas, pesos):
         self.entradas = entradas
-        self.rede = rede
         self.pesos = pesos
+        self.rede = rede
 
     def iniciar(self):
-        self.entradas, self.pesos = RedeNeural.rede(self)
-        return {'saida': self.entradas, 'pesos': self.pesos}
+        '''Inicia uma rede neural'''
 
-    def n(peso, entradas):
+        return RedeNeural.rede(self)
+
+    def neuronio(peso, entradas):
+        '''Neurônio individual'''
+
         resultado = sum(entradas) * sqrt(peso ** 2)
         return resultado
 
     def rede(self):
+        '''Montador da rede neural'''
+
+        # Ande por todas as camadas
         for camada in range(0, len(self.rede)):
             novasEntradas = []
 
+            # Ande pela quantidade de neurônios da camada
             for neuronio in range(0, self.rede[camada]):
                 novasEntradas.append(
-                    RedeNeural.n(
+
+                    # Aplique em o resultado em um neurônio
+                    RedeNeural.neuronio(
                         self.pesos[camada][neuronio],
                         self.entradas))
 
             self.entradas = novasEntradas
-        return self.entradas, self.pesos
-'''
-rede = [2,2,1]
-entradas = [16,32]
-pesos = [[-6.556, 3.89], [2.121, 3.149],[-0.025]]
-n = RedeNeural(rede,entradas,pesos)
-print(n.iniciar())
-'''
+
+        # Retorne os pesos usados e o resultado
+        return self.entradas
+
+class Treino():
+    def __init__(self,rede, intervalorMenor, intervaloMaior, esperado, entradas, geracoes
+):
+        self.intervalorMenor = intervalorMenor
+        self.intervaloMaior = intervaloMaior
+        self.esperado = esperado
+        self.entradas = entradas
+        self.geracoes = geracoes
+        self.rede = rede
+
+    def melhorRede(self, resultados, esperado):
+        melhorResultadoPeso = []
+
+        for resultado in resultados:
+
+            if melhorResultadoPeso == []:
+                melhorResultadoPeso = resultado
+            else:
+
+                maior = resultado['saida'][0]
+                menor = esperado[0]
+
+                if menor > maior:
+                    a = maior
+                    maior = menor
+                    menor = a
+
+                teste = maior - menor
+
+                if melhorResultadoPeso['saida'][0] > teste:
+                    melhorResultadoPeso = resultado
+
+            return melhorResultadoPeso
+
+    def gerarPesosAleatorios(self):
+        pesos = []
+
+        for camada in self.rede:
+
+            pesosCamada = []
+            for neuronio in range(0, camada):
+
+                sorteado = randint(self.intervalorMenor * 1000, self.intervaloMaior * 1000)
+                sorteado = sorteado / 1000
+
+                pesosCamada.append(sorteado)
+
+            pesos.append(pesosCamada)
+
+        return pesos
+
+    def treinarRede(self):
+
+        resultados = []
+
+        for geracao in range(0, self.geracoes):
+            pesos = Treino.gerarPesosAleatorios(self)
+
+            neuronio = RedeNeural(self.rede, self.entradas, pesos)
+            resultados.append(neuronio.iniciar())
+
+        return Treino.melhorRede(self, resultados, self.esperado)
+
+
+
+class Otimizar():
+    def __init__(self, rede, esperado, entradas, geracoes, melhorPeso, otimizacaoPor = 1):
+        self.otimizacaoPor = otimizacaoPor
+        self.melhorPeso = melhorPeso
+        self.esperado = esperado
+        self.entradas = entradas
+        self.geracoes = geracoes
+        self.rede = rede
+
+    def melhorRede(self, resultados):
+        melhorResultadoPeso = []
+
+        for resultado in resultados:
+
+            if melhorResultadoPeso == []:
+                melhorResultadoPeso = resultado
+            else:
+
+                maior = resultado['saida'][0]
+                menor = self.esperado[0]
+
+                if menor > maior:
+                    a = maior
+                    maior = menor
+                    menor = a
+
+                teste = maior - menor
+
+                if melhorResultadoPeso['saida'][0] > teste:
+                    melhorResultadoPeso = resultado
+
+        return melhorResultadoPeso
+
+    def otimizarPesosAleatorios(self, melhorPeso):
+
+        pesos = []
+
+        for camada in range(0, len(self.rede)):
+
+            pesosCamada = []
+            for neuronio in range(0, self.rede[camada]):
+
+                if melhorPeso[camada][neuronio] < 0:
+                    menor = melhorPeso[camada][neuronio] + (
+                        melhorPeso[camada][neuronio] / self.otimizacaoPor)
+                    maior = melhorPeso[camada][neuronio] - (melhorPeso[
+                        camada][neuronio] / self.otimizacaoPor)
+                else:
+                    menor = melhorPeso[camada][neuronio] - (melhorPeso[
+                        camada][neuronio] / self.otimizacaoPor)
+                    maior = melhorPeso[camada][neuronio] + (melhorPeso[
+                        camada][neuronio] / self.otimizacaoPor)
+
+                sorteado = randint(int(menor * 1000), int(maior * 1000))
+                pesosCamada.append(sorteado / 1000)
+            pesos.append(pesosCamada)
+
+        return pesos
+
+    def otimizarRede(self):
+        if self.otimizacaoPor == 0:
+            self.otimizacaoPor = 1
+
+        resultados = []
+
+        for geracao in range(0, self.geracoes):
+
+            pesos = Otimizar.otimizarPesosAleatorios(self, self.melhorPeso)
+            neuronio = RedeNeural(self.rede, self.entradas, pesos)
+            resultados.append({'saida': neuronio.iniciar(), 'pesos': pesos})
+
+       
+        return Otimizar.melhorRede(self, resultados)
+
+
